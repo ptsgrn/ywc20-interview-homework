a994d0a29b9fe7234549f0cc13110ff90f7b939b
Patsagorn <patsagorn_y@outlook.com>
2025-05-04 20:28:36 +0700

    ci: add Github Page workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  site: 'https://ptsgrn.github.io',
+  base: 'ywc20-interview-homework',
+  integrations: [svelte()],
 });
